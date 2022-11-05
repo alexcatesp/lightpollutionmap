2c077abced59a3121c66c3fbce447c602a6dd224
alejandrocatalaiesgalileo <alejandroce@iesgalileo.es>
2022-11-05 01:24:35 +0100

    moved jquery lib

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Light Pollution Map </title>
     <link rel="stylesheet" href="./src/css/style.css">
-    <script src="./src/js/index.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="./src/js/index.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsneVM_MSta--x2Sc-j1A55ZJDKPOAFVg&v=weekly" defer></script>
 </head>
 
